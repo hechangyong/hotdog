ddd4b001c7171d587f9c62f700953f1b2ada58b0
hecy <hechangyong@aliyun.com>
2018-08-13 15:42:04 +0800

    代码

---
+++ src/main/java/com/hotdog/hotdog/HotdogApplication.java
@@ -0,0 +1,12 @@
+package com.hotdog.hotdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotdogApplication.class, args);
+	}
+}
